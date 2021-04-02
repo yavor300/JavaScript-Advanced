abf113eb103711f9f5b81731903afae0f91bfd7b
Yavor Chamov <yavor300@gmail.com>
2021-04-02 12:03:34 +0300

    Fourth problem: Circle Area

---
+++ 01-Syntax-Functions-and-Statements/Lab/circle-area.js
@@ -0,0 +1,7 @@
+function solve(radius) {
+    if (typeof radius === 'number') {
+        console.log((Math.PI * radius * radius).toFixed(2));
+    } else {
+        console.log('We can not calculate the circle area, because we receive a ' + typeof radius + '.');
+    }
+}
