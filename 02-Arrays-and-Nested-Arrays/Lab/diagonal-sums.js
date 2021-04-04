3fc09490fb5b77ee9375d7fa269f9a34a9a71d80
Yavor Chamov <yavor300@gmail.com>
2021-04-04 18:34:41 +0300

    10.	Diagonal Sums

---
+++ 02-Arrays-and-Nested-Arrays/Lab/diagonal-sums.js
@@ -0,0 +1,19 @@
+function solve(matrix) {
+    let primary = 0;
+    let secondary = 0;
+
+    for (let row = 0; row < matrix.length; row++) {
+        primary += matrix[row][row];
+    }
+
+    for (let row = 0; row < matrix.length; row++) {
+        secondary += matrix[row][matrix[row].length - 1 - row];       
+    }
+
+    console.log(`${primary} ${secondary}`);
+}
+
+solve([[3, 5, 17],
+    [-1, 7, 14],
+    [1, -8, 89]]);
+
