d232140d1063484d43167e07802b2c986d388d96
Yavor Chamov <yavor300@gmail.com>
2021-04-09 22:48:03 +0300

    Update app.js

--- 05-DOM-Manipulations-and-Events/Lab/02. Add-Delete/app.js
+++ 05-DOM-Manipulations-and-Events/Lab/02. Add-Delete/app.js
@@ -1,25 +1,47 @@
 function addItem() {
-    const input = document.getElementById('newItemText').value;
+    // const input = document.getElementById('newItemText').value;
 
-    if (input.length !== 0) {
-        const li = document.createElement('li');
-        li.textContent = input;
+    // if (input.length !== 0) {
+    //     const li = document.createElement('li');
+    //     li.textContent = input;
 
-        const a = document.createElement('a');
-        a.textContent = '[Delete]';
-        a.href = '#';
-        li.appendChild(a);
+    //     const a = document.createElement('a');
+    //     a.textContent = '[Delete]';
+    //     a.href = '#';
+    //     li.appendChild(a);
         
-        document.getElementById('items').appendChild(li);
+    //     document.getElementById('items').appendChild(li);
     
-        Array.from(li.children)[0].addEventListener('click', remove);
+    //     Array.from(li.children)[0].addEventListener('click', remove);
     
-        document.getElementById('newItemText').value = '';
-    }
+    //     document.getElementById('newItemText').value = '';
+    // }
 
     
 
-    function remove(e) {
-        e.target.parentNode.remove();
+    // function remove(e) {
+    //     e.target.parentNode.remove();
+    // }
+
+
+    const input = document.getElementById('newItemText');
+    const liElement = createElement('li', input.value);
+
+
+    const deleteBtn = createElement('a', '[Delete]');
+    deleteBtn.href = '#';
+    deleteBtn.addEventListener('click', (ev) => {
+        ev.target.parentNode.remove();
+    })
+    liElement.appendChild(deleteBtn);
+
+    document.getElementById('items').appendChild(liElement);
+    input.value = '';
+
+
+    function createElement(type, content) {
+        const element = document.createElement(type);
+        element.textContent = content;
+        return element;
     }
 }
