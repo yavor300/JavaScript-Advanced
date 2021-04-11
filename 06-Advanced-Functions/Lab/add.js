b4da6fc5e44716a5cbc2d5d8a7618aa06a998b38
Yavor Chamov <yavor300@gmail.com>
2021-04-11 22:00:01 +0300

    2.	Add

---
+++ 06-Advanced-Functions/Lab/add.js
@@ -0,0 +1,13 @@
+function solution(number) {
+    
+    return add;
+
+    function add(increment) {
+        return number + increment;
+    }
+}
+
+
+let add5 = solution(5);
+console.log(add5(2));
+console.log(add5(3));
