0cce754ea438d4574d3529dff320770b85f3158e
Yavor Chamov <yavor300@gmail.com>
2021-04-18 21:51:57 +0300

    7.	HEX

---
+++ 08-Classes/Exercises/hex.js
@@ -0,0 +1,33 @@
+class Hex {
+    constructor(value) {
+        this.value = value;
+    }
+
+    valueOf() {
+        return this.value;
+    }
+
+    toString() {
+        return '0x' + this.value.toString(16).toUpperCase();
+    }
+
+    plus(number) {
+        if (number instanceof Hex) {
+            return new Hex(this.value + number.value);
+        } else {
+            return new Hex(this.value + number);
+        }
+    }
+
+    minus(number) {
+        if (number instanceof Hex) {
+            return new Hex(this.value - number.value);
+        } else {
+            return new Hex(this.value - number);
+        }
+    }
+
+    static parse(string) {
+        return parseInt(string, 16);
+    }
+}
