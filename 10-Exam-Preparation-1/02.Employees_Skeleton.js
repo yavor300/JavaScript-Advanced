56094df9905d306c4295fd2ace23d38c49147424
Yavor Chamov <yavor300@gmail.com>
2021-04-21 23:08:43 +0300

    Problem 2. Employees

---
+++ 10-Exam-Preparation-1/02.Employees_Skeleton.js
@@ -0,0 +1,123 @@
+function solveClasses() {
+
+    class Developer {
+        constructor(firstName, lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.baseSalary = 1000;
+            this.tasks = [];
+            this.experience = 0;
+        }
+
+        addTask(id, taskName, priority) {
+            if (priority === 'high') {
+                this.tasks.unshift({id, taskName, priority});
+            } else {
+                this.tasks.push({id, taskName, priority});
+            }
+
+            return `Task id ${id}, with ${priority} priority, has been added.`;
+        }
+
+        doTask() {
+            if (this.tasks.length === 0) {
+                return `${this.firstName}, you have finished all your tasks. You can rest now.`
+            } else {
+                return this.tasks.shift().taskName;
+            }
+        }
+
+        getSalary() {
+            return `${this.firstName} ${this.lastName} has a salary of: ${this.baseSalary}`
+        }
+
+        reviewTasks() {
+            let result = `Tasks, that need to be completed:\n`;
+            this.tasks.forEach(task => {
+                result += `${task.id}: ${task.taskName} - ${task.priority}\n`;
+            });
+            
+            return result.trim();
+        }
+    }
+
+    class Junior extends Developer {
+        constructor(firstName, lastName, bonus, experience) {
+            super(firstName, lastName);
+            this.bonus = bonus;
+            this.experience = experience;
+            this.baseSalary = 1000 + bonus;
+        }
+
+        learn(years) {
+            this.experience = this.experience + years;
+        }
+    }
+
+    class Senior extends Developer {
+        constructor(firstName, lastName, bonus, experience) {
+            super(firstName, lastName);
+            this.bonus = bonus;
+            this.experience = experience;
+            this.baseSalary = 1000 + bonus;
+            this.experience = experience + 5;
+        }
+
+        changeTaskPriority(taskId) {
+            this.tasks.forEach((taskEl, i) => {
+                if (taskEl.id === taskId) {
+                    const task = taskEl;
+                    const priority = task.priority;
+                    if (priority === 'high') {
+                        task.priority = 'low';
+                        this.tasks.splice(i, 1);
+                        this.tasks.push(task);
+                    } else {
+                        task.priority = 'high';
+                        this.tasks.splice(i, 1);
+                        this.tasks.unshift(task);
+                    }
+                    return task;
+                }
+            });
+        }
+    }
+
+
+    return {
+        Developer,
+        Junior,
+        Senior
+    }
+}
+
+//Zero test 1 Developer
+
+const classes = solveClasses();
+const developer = new classes.Developer("Jonathan", "Joestar");
+
+// Salary
+const salaryResult = developer.getSalary();
+console.log(salaryResult);
+const salaryExpect = "Jonathan Joestar has a salary of: 1000";
+// expect(salaryResult).to.be.eq(salaryExpect);
+
+// // Add task
+// const addTaskResult = developer.addTask(1, "Inspect bug", "low");
+// const addTaskExpect = "Task id 1, with low priority, has been added.";
+// expect(addTaskResult).to.be.eq(addTaskExpect);
+
+
+// // Review tasks
+// developer.addTask(2, "Update repository", "high");
+// const reviewTasksResult = developer.reviewTasks();
+// const reviewTasksExpect = "Tasks, that need to be completed:\n2: Update repository - high\n1: Inspect bug - low";
+// expect(reviewTasksResult).to.be.eq(reviewTasksExpect);
+
+
+// //Do task
+// developer.doTask();
+// developer.doTask();
+// const doTaskResult = developer.doTask();
+// const doTaskExpect = "Jonathan, you have finished all your tasks. You can rest now.";
+// expect(doTaskResult).to.be.eq(doTaskExpect);
