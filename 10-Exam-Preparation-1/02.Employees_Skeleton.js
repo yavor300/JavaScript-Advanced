e007ffe42286fbbe3e583cfa86df8e6e9855c127
Yavor Chamov <yavor300@gmail.com>
2021-04-22 09:15:22 +0300

    Update 02.Employees_Skeleton.js

--- 10-Exam-Preparation-1/02.Employees_Skeleton.js
+++ 10-Exam-Preparation-1/02.Employees_Skeleton.js
@@ -90,34 +90,3 @@ function solveClasses() {
         Senior
     }
 }
-
-//Zero test 1 Developer
-
-const classes = solveClasses();
-const developer = new classes.Developer("Jonathan", "Joestar");
-
-// Salary
-const salaryResult = developer.getSalary();
-console.log(salaryResult);
-const salaryExpect = "Jonathan Joestar has a salary of: 1000";
-// expect(salaryResult).to.be.eq(salaryExpect);
-
-// // Add task
-// const addTaskResult = developer.addTask(1, "Inspect bug", "low");
-// const addTaskExpect = "Task id 1, with low priority, has been added.";
-// expect(addTaskResult).to.be.eq(addTaskExpect);
-
-
-// // Review tasks
-// developer.addTask(2, "Update repository", "high");
-// const reviewTasksResult = developer.reviewTasks();
-// const reviewTasksExpect = "Tasks, that need to be completed:\n2: Update repository - high\n1: Inspect bug - low";
-// expect(reviewTasksResult).to.be.eq(reviewTasksExpect);
-
-
-// //Do task
-// developer.doTask();
-// developer.doTask();
-// const doTaskResult = developer.doTask();
-// const doTaskExpect = "Jonathan, you have finished all your tasks. You can rest now.";
-// expect(doTaskResult).to.be.eq(doTaskExpect);
